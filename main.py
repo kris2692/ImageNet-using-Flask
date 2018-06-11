5142f1b6029555778b6a419427e3b9a20ece4737
kris2692 <krishnasree2692@gmail.com>
2018-06-10 21:57:02 -0400

    Version 1.0

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ from keras.applications.resnet50 import ResNet50
 from keras.preprocessing import image
 from keras.applications.resnet50 import preprocess_input, decode_predictions
 import numpy as np
+import os
 from flask import Flask, request, render_template, redirect, url_for, flash
-
+from werkzeug import secure_filename
 
 UPLOAD_FOLDER=r"D:\Flask-Image_net\uploads"
 app = Flask(__name__)
@@ -13,16 +14,19 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def take_input():
   if request.method=='POST':
     model = ResNet50(weights='imagenet')
-    img_path = request.form.get('picture')
-    print(img_path)
-    img = image.load_img(img_path, target_size=(224,224))
+    image1 = request.files['picture']
+    filename=secure_filename(image1.filename)
+    image1.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#    image_path=open(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(image1.filename)),mode="rb")
+    print(image1)
+    img = image.load_img(image1, target_size=(224,224))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
     preds = model.predict(x)
 # decode the results into a list of tuples (class, description, probability)
 # (one such list for each sample in the batch)
-    print('Predicted:', decode_predictions(preds, top=10)[0])
+    print('Predicted:', decode_predictions(preds, top=3)[0])
   return render_template('home.html')
 
 if __name__=='__main__':
